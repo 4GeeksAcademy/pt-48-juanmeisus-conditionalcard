683e5411843b334ce64cb8d867331aae457a1d38
juan meisus <130049281+juanmeisus@users.noreply.github.com>
2023-10-14 18:43:05 +0000

    guardar

--- src/js/app.js
+++ src/js/app.js
@@ -34,7 +34,7 @@ function render(variables = {}) {
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
           <h1> ${variables.name} ${variables.lastName}</h1>
-          <h2>Web Developer</h2>
+          <h2>${variables.role}</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
